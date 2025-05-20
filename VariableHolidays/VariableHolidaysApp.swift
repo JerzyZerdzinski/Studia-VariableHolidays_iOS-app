094f987de03c526fe7183c1c6cd23940134c2a7d
jurek <jerzyzerdzinski@gmail.com>
2025-05-20 15:15:52 +0200

    Initial Commit

---
+++ VariableHolidays/VariableHolidaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  VariableHolidaysApp.swift
+//  VariableHolidays
+//
+//  Created by Jerzy Żerdziński on 20/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct VariableHolidaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
